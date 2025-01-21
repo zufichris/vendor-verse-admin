a875d420624e2612bd762122fa955037c183c03d
{zufichris} <{zufichris@gmail.com}>
2025-01-21 12:53:39 +0400

    Add request handler

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export async function request<T>(path = '/', init?: RequestInit): Promise<T | null> {
+  try {
+    const res = await fetch(`${process.env.NEXT_ADMIN_BASE_URL}${path}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "GET",
+      ...init,
+    })
+    const data = await res.json()
+    return data as T
+  } catch (error) {
+    throw error
+  }
+}
