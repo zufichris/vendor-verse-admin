4ee11ce394f3130c9278550adaea41f34eadd03f
zufichris <zufichris@gmail.com>
2025-01-25 23:59:13 +0400

    UPdated Utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { getToken } from "./actions/auth"
+import { IResponseData } from "./types/global"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-export async function request<T>(path = '/', init?: RequestInit): Promise<T | null> {
+export async function request<T = IResponseData<unknown>>(path = '/', init?: RequestInit): Promise<T> {
   try {
-    const url = new URL(`api/v1/${path}`, process.env.NEXT_ADMIN_BASE_URL)
-    console.log(url, "URL")
+    const url = new URL(`/api/v1/${path}`, process.env.NEXT_PUBLIC_ADMIN_BASE_URL).toString()
+    const access_token = await getToken("access_token")
     const res = await fetch(url, {
       headers: {
         "Content-Type": "application/json",
+        authorization: access_token ? `Bearer ${access_token}` : "",
       },
       method: "GET",
       ...init,
