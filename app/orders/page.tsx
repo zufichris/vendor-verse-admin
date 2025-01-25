dadf847d7eddc0e04471fd8bb81564197cbfa32e
zufichris <zufichris@gmail.com>
2025-01-26 00:04:47 +0400

    Updated Orders page

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,18 +1,13 @@
-import { DashboardShell } from "@/components/dashboard-shell"
-import { DashboardHeader } from "@/components/dashboard-header"
 import { OrdersList } from "@/components/orders/orders-list"
 import { OrdersHeader } from "@/components/orders/order-header"
 
 
 export default function OrdersPage() {
   return (
-    <DashboardShell>
-      <div className="flex flex-col gap-6 w-full pt-0">
-        <DashboardHeader />
-        <OrdersHeader />
-        <OrdersList />
-      </div>
-    </DashboardShell>
+    <div className="flex flex-col gap-6 w-full pt-0">
+      <OrdersHeader />
+      <OrdersList />
+    </div>
   )
 }
 
