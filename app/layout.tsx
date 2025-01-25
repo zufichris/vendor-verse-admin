332f632e3244513ac696ace177ea555249f60d9b
zufichris <zufichris@gmail.com>
2025-01-26 00:01:22 +0400

    Updated Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,26 +3,31 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ToastProvider } from "@/components/providers/toast-provider"
+import { DashboardShell } from "@/components/dashboard-shell"
+import { getLoggedInUser } from "@/lib/actions/user"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "VendorVerse Admin",
-  description: "Multivendor E-commerce Admin Dashboard",
+  description: "Multi-vendor E-commerce Admin Dashboard",
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const res = await getLoggedInUser()
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} thin-scrollbar min-h-screen bg-gradient-to-b from-background to-muted/20 dark:from-background dark:to-muted/20`}
       >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
+          {res?.success ? <DashboardShell user={res?.data} success={res.success}>
+            {children}
+          </DashboardShell> : <>{children}</>}
           <ToastProvider />
         </ThemeProvider>
       </body>
