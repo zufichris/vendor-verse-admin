1d5a0091eb70fec6e1d031006b1da4e60a35ff70
zufichris <zufichris@gmail.com>
2025-01-26 00:05:04 +0400

    Updated Home page loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,3 @@
-import { DashboardShell } from "@/components/dashboard-shell"
-import { DashboardHeader } from "@/components/dashboard-header"
 import {
   OverviewSkeleton,
   RecentOrdersSkeleton,
@@ -9,19 +7,16 @@ import {
 
 export default function DashboardLoading() {
   return (
-    <DashboardShell>
-      <div className="flex flex-col gap-6 w-full pt-0">
-        <DashboardHeader />
-        <div className="grid gap-6 grid-cols-1 xs:grid-cols-2 lg:grid-cols-4 w-full">
-          <OverviewSkeleton />
-        </div>
-        <div className="w-full">
-          <TopProductsSkeleton />
-          <VendorMetricsSkeleton />
-        </div>
-        <RecentOrdersSkeleton />
+    <div className="flex flex-col gap-6 w-full pt-0">
+      <div className="grid gap-6 grid-cols-1 xs:grid-cols-2 lg:grid-cols-4 w-full">
+        <OverviewSkeleton />
       </div>
-    </DashboardShell>
+      <div className="w-full">
+        <TopProductsSkeleton />
+        <VendorMetricsSkeleton />
+      </div>
+      <RecentOrdersSkeleton />
+    </div>
   )
 }
 
