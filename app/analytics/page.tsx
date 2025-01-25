52d7c93968d348e9e51998381dca80b26c5e24fe
zufichris <zufichris@gmail.com>
2025-01-26 00:04:31 +0400

    Updated Analytics page

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,19 +1,14 @@
-import { DashboardShell } from "@/components/dashboard-shell"
-import { DashboardHeader } from "@/components/dashboard-header"
 import { AnalyticsHeader } from "@/components/analytics/analytics-header"
 import { AnalyticsCharts } from "@/components/analytics/analytics-charts"
 import { AnalyticsStats } from "@/components/analytics/analytics-stats"
 
 export default function AnalyticsPage() {
   return (
-    <DashboardShell>
-      <div className="flex flex-col gap-6 w-full pt-0">
-        <DashboardHeader />
+    <div className="flex flex-col gap-6 w-full pt-0">
         <AnalyticsHeader />
         <AnalyticsStats />
         <AnalyticsCharts />
       </div>
-    </DashboardShell>
   )
 }
 
