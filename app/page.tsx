b0227146bfe1dab205f79c2838828353d9011d59
zufichris <zufichris@gmail.com>
2025-01-26 00:01:38 +0400

    Updated Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,20 @@
-import { DashboardShell } from "@/components/dashboard-shell"
-import { DashboardHeader } from "@/components/dashboard-header"
 import { Overview } from "@/components/overview"
 import { RecentOrders } from "@/components/recent-orders"
 import { TopProducts } from "@/components/top-products"
 import { VendorMetrics } from "@/components/vendor-metrics"
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
   return (
-    <DashboardShell>
-      <div className="flex flex-col gap-6 w-full pt-0">
-        <DashboardHeader />
-        <div className="grid gap-6 grid-cols-1 xs:grid-cols-2 lg:grid-cols-4 w-full">
-          <Overview />
-        </div>
-        <div className="w-full space-y-6">
-          <TopProducts />
-          <VendorMetrics />
-        </div>
-        <RecentOrders />
+    <div className="flex flex-col gap-6 w-full pt-0">
+      <div className="grid gap-6 grid-cols-1 xs:grid-cols-2 lg:grid-cols-4 w-full">
+        <Overview />
       </div>
-    </DashboardShell>
+      <div className="w-full space-y-6">
+        <TopProducts />
+        <VendorMetrics />
+      </div>
+      <RecentOrders />
+    </div>
   )
 }
 
