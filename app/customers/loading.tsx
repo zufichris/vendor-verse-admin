b22405446a58f7111e1c7e1dcfbac77897a2daae
zufichris <zufichris@gmail.com>
2025-01-26 00:04:17 +0400

    Updated Customers page

--- app/customers/loading.tsx
+++ app/customers/loading.tsx
@@ -1,26 +1,19 @@
-import { DashboardShell } from "@/components/dashboard-shell"
-import { DashboardHeader } from "@/components/dashboard-header"
 import { CustomersTableSkeleton } from "@/components/customers/customers-skeleton"
 import { Skeleton } from "@/components/ui/skeleton"
 
 export default function CustomersLoading() {
   return (
-    <DashboardShell>
-      <div className="flex flex-col gap-6 w-full pt-0">
-        <DashboardHeader />
-        <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-          <div className="space-y-1">
-            <Skeleton className="h-8 w-[200px]" />
-            <Skeleton className="h-4 w-[300px]" />
-          </div>
-          <div className="flex flex-col sm:flex-row gap-4">
-            <Skeleton className="h-10 w-full sm:w-[300px]" />
-            <Skeleton className="h-10 w-full sm:w-[120px]" />
-          </div>
-        </div>
-        <CustomersTableSkeleton />
+    <div className="flex flex-col gap-6 w-full pt-0">
+      <div className="space-y-1">
+        <Skeleton className="h-8 w-[200px]" />
+        <Skeleton className="h-4 w-[300px]" />
       </div>
-    </DashboardShell>
+      <div className="flex flex-col sm:flex-row gap-4">
+        <Skeleton className="h-10 w-full sm:w-[300px]" />
+        <Skeleton className="h-10 w-full sm:w-[120px]" />
+      </div>
+      <CustomersTableSkeleton />
+    </div>
   )
 }
 
--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,17 +1,12 @@
-import { DashboardShell } from "@/components/dashboard-shell"
-import { DashboardHeader } from "@/components/dashboard-header"
 import { CustomersList } from "@/components/customers/customers-list"
 import { CustomersHeader } from "@/components/customers/customers-header"
 
-export default function CustomersPage() {
+export default async function CustomersPage() {
   return (
-    <DashboardShell>
-      <div className="flex flex-col gap-6 w-full pt-0">
-        <DashboardHeader />
-        <CustomersHeader />
-        <CustomersList />
-      </div>
-    </DashboardShell>
+    <div className="flex flex-col gap-6 w-full pt-0">
+      <CustomersHeader />
+      <CustomersList />
+    </div>
   )
 }
 
