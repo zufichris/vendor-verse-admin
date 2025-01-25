1b5ce028cc743dd759a9d456b8d9f4aaf8967fb2
zufichris <zufichris@gmail.com>
2025-01-26 00:03:46 +0400

    Updated Settiongs page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,19 +1,14 @@
-import { DashboardShell } from "@/components/dashboard-shell"
-import { DashboardHeader } from "@/components/dashboard-header"
 import { SettingsTabs } from "@/components/settings/settings-tabs"
 
 export default function SettingsPage() {
   return (
-    <DashboardShell>
-      <div className="flex flex-col gap-6 w-full pt-0">
-        <DashboardHeader />
+    <div className="flex flex-col gap-6 w-full pt-0">
         <div>
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">Manage your account settings</p>
         </div>
         <SettingsTabs />
       </div>
-    </DashboardShell>
   )
 }
 
