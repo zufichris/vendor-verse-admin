05e55f4fef49510cebb6887a8dce5a422a76e2b9
zufichris <zufichris@gmail.com>
2025-01-27 21:43:34 +0400

    customers  page

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,11 +1,23 @@
-import { CustomersList } from "@/components/customers/customers-list"
 import { CustomersHeader } from "@/components/customers/customers-header"
+import { CustomersList } from "@/components/customers/customers-list"
+import { ErrorMessage } from "@/components/ui/error"
+import { getCustomers } from "@/lib/actions/user"
 
-export default async function CustomersPage() {
+interface CustomerPageProps {
+  readonly searchParams: string
+}
+
+export default async function CustomersPage({ searchParams }: CustomerPageProps) {
+  const qs = new URLSearchParams(searchParams).toString()
+  const res = await getCustomers(qs)
+  if (!res.success) return <ErrorMessage message={res.message} description={res.description} status={res.status} />
+
   return (
-    <div className="flex flex-col gap-6 w-full pt-0">
-      <CustomersHeader />
-      <CustomersList />
+    <div className="flex flex-col gap-4 sm:gap-6 w-full pt-0 px-4 sm:px-6">
+      <CustomersHeader
+        {...res}
+      />
+      <CustomersList customers={res.data} />
     </div>
   )
 }
