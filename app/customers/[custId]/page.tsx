54b4695d7c3d888c2e4e3f04186a2c4c6b1a3c04
zufichris <zufichris@gmail.com>
2025-01-28 21:39:18 +0400

    Updated customer pages

--- app/customers/[custId]/page.tsx
+++ app/customers/[custId]/page.tsx
@@ -1,12 +1,38 @@
 import CustomerDetails from '@/components/customers/customer-details'
+import { ErrorMessage } from '@/components/ui/error'
+import { getCustomerById } from '@/lib/actions/user'
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 import React from 'react'
-import { promisify } from 'util'
 
-
-export default async function CustomerDetailsPage() {
-  await promisify(setTimeout)(3000)
-  return (
-    <CustomerDetails />
-  )
+interface PageProps {
+  params: {
+    custId: string
+  }
 }
 
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { custId } = params
+  const res = await getCustomerById(custId)
+  if (!res.success) {
+    if (res.status === 400) {
+      return notFound()
+    }
+    return {
+      title: `${res.status}-${res.message}`,
+      description: res.description,
+    }
+  }
+  return {
+    title: res?.data?.firstName,
+    description: `${res.data.firstName} has ${res.data?.stats?.totalOrders ?? 0} orders`,
+  }
+}
+
+export default async function CustomerDetailsPage({ params }: PageProps) {
+  const { custId } = params
+  const res = await getCustomerById(custId)
+  if (!res.success)
+    return <ErrorMessage message={res.message} description={res.description} status={res.status} />
+  return <CustomerDetails customer={res.data} />
+}
