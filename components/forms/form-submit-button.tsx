6a8b6836d377fd4270e775e3f6a9191dfc1234bc
zufichris <zufichris@gmail.com>
2025-01-28 21:41:27 +0400

    Updated form comp

--- components/forms/form-submit-button.tsx
+++ components/forms/form-submit-button.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { Loader2 } from "lucide-react"
 import { useFormStatus } from "react-dom"
 
 export function FormSubmitButton({ children }: { children: React.ReactNode }) {
@@ -9,7 +8,6 @@ export function FormSubmitButton({ children }: { children: React.ReactNode }) {
 
   return (
     <Button type="submit" disabled={pending} isLoading={pending}>
-      {pending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
       {children}
     </Button>
   )
