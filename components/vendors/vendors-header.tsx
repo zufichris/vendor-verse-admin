2e7b285fa178300035310997602e56405ab37489
zufichris <zufichris@gmail.com>
2025-08-08 07:11:24 +0400

    refactor(vendors): remove unused vendor pages and UI components

--- components/vendors/vendors-header.tsx
+++
@@ -1,6 +0,0 @@
-// Update the import section to include the new modal
-import { AddVendorModal } from "./add-vendor-modal"
-
-// Replace the Button in the return statement with the modal
-;<AddVendorModal />
-
